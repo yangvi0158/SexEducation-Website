9bb6bbaad39768bde43eabe1d11297dcf8802b27
viboloveyou12 <viboloveyou12@gmail.com>
2021-02-18 15:37:23 +0800

    object: 1.加入vue-loading套件  2. preload 首頁img

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="home" 
   @click="changeColor"
-  :class="{'backPurple':backgroundClass.isA,
+  :class="{
+  'backPurple':backgroundClass.isA,
   'backWhite':backgroundClass.isB,
   'backBlack':backgroundClass.isC}"
   >
+    <vue-element-loading :active="isDelay" color="#FFFFFF" background-color="#CCAFEF" spinner="line-wave"/>
     <img id="homeImg" ref="homeImg" :src="largeImgUrl">
     <img class="changeColor" :src="smallImgUrl" :style="imgStyle">
   </div>
 </template>
 
 <script>
+import VueElementLoading from "vue-element-loading";
 export default {
   name: 'Home',
+  components: {
+    VueElementLoading
+  },
   data () {
     return {
+      isDelay: true,
       offsetLeft: 0,
       offsetTop: 0,
       innerLeft: 0,
@@ -22,6 +29,20 @@ export default {
       scalePercent: 0,
       largeImgUrl: require("../assets/img/homePurple.png"),
       smallImgUrl: require("../assets/img/changeWhite.png"),
+      img: {
+        backgroundA: {
+          large: require("../assets/img/homeWhite.png"),
+          small: require("../assets/img/changeBlack.png")
+        },
+        backgroundB: {
+          large: require("../assets/img/homeBlack.png"),
+          small: require("../assets/img/changePurple.png")
+        },
+        backgroundC: {
+          large: require("../assets/img/homePurple.png"),
+          small: require("../assets/img/changeWhite.png")
+        }
+      },
       backgroundClass:{
         isA: true,
         isB: false,
@@ -40,25 +61,26 @@ export default {
     //取得圈圈大小
     getscalePercent(event){
       this.scalePercent = homeImg.clientWidth*.075;
+      this.isDelay = false
     },
     //首頁與圈圈換圖
     changeColor(){
       if(this.backgroundClass.isA === true){
         this.$emit("homecolor-changenav", '2');
-        this.largeImgUrl = require("../assets/img/homeWhite.png");
-        this.smallImgUrl = require("../assets/img/changeBlack.png");
+        this.largeImgUrl = this.img.backgroundA.large
+        this.smallImgUrl = this.img.backgroundA.small
         this.backgroundClass.isA = false;
         this.backgroundClass.isB = true;
       }else if(this.backgroundClass.isB === true){
         this.$emit("homecolor-changenav", '3');
-        this.largeImgUrl = require("../assets/img/homeBlack.png");
-        this.smallImgUrl = require("../assets/img/changePurple.png");
+        this.largeImgUrl = this.img.backgroundB.large
+        this.smallImgUrl = this.img.backgroundB.small
         this.backgroundClass.isB = false;
         this.backgroundClass.isC = true;
       }else if(this.backgroundClass.isC === true){
         this.$emit("homecolor-changenav", '1');
-        this.largeImgUrl = require("../assets/img/homePurple.png");
-        this.smallImgUrl = require("../assets/img/changeWhite.png");
+        this.largeImgUrl = this.img.backgroundC.large
+        this.smallImgUrl = this.img.backgroundC.small
         this.backgroundClass.isA = true;
         this.backgroundClass.isC = false;
       }
@@ -66,15 +88,18 @@ export default {
   },
   mounted(){
     //圈圈位置與大小變動監聽
-    this.$nextTick(function(){
-      window.addEventListener('resize', this.getDistance);
-      window.addEventListener('load', this.getDistance);
-      window.addEventListener('resize',this.getscalePercent);
-      window.addEventListener('load',this.getscalePercent);
+    this.isDelay = true;
+    setTimeout(()=>{
+        this.$nextTick(function(){
+          window.addEventListener('resize', this.getDistance);
+          window.addEventListener('load', this.getDistance);
+          window.addEventListener('resize',this.getscalePercent);
+          window.addEventListener('load',this.getscalePercent);
 
-      this.getDistance();
-      this.getscalePercent();
-    })
+          this.getDistance();
+          this.getscalePercent();
+        })
+    }, 500)
   },
   beforeDestroy(){
       // window.removeEventListener('load', this.getDistance);
